c494a272917c8bf1e100cf5526a8f5f7efd07a1e
phfbonini <phf_apps@outlook.com>
2023-05-03 17:28:17 -0300

    Utilizando o App.js como component principal, nele há um Navbar que utiliza a lib de router-dom para navegar entre os components: CurrencyConverter, SearchHistory e QuotesAvailable Estilização padrao do create-react-app apagada, implementado apenas o background com cor passando as alterações do packager.json

--- frontend/cotas-moedas/src/App.js
+++ frontend/cotas-moedas/src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
-import CurrencyConverter from './CurrencyConverter';
+import './App.css'; // importando o arquivo CSS
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import CurrencyConverter from './Components/CurrencyConverter';
+import SearchHistory from './Components/SearchHistory';
+import QuotesAvailable from './Components/QuotesAvailable';
 
 function App() {
   return (
-    <Container>
-      <CurrencyConverter />
-    </Container>
+    <Router>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">Cotações</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/historico">Histórico de Pesquisas</Nav.Link>
+            <Nav.Link as={Link} to="/disponiveis">Moedas Disponiveis</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Container className="my-4">
+        <Routes>
+          <Route path="/" element={<CurrencyConverter />} />
+          <Route path="/historico" element={<SearchHistory />} />
+          <Route path="/disponiveis" element={<QuotesAvailable />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
